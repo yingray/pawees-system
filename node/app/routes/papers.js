2dbde932b98c2b06075ee7ba1f176b17a530d19a
Ying-Ray Lu <yingray.lu@fuhu.com>
2017-09-03 22:59:00 +0800

    update paper features

--- node/app/routes/papers.js
+++ node/app/routes/papers.js
@@ -1,5 +1,12 @@
 import Admin from '../models/Admin'
 import Registration from '../models/Registration'
+import requestBodyToModelBody, { firstAuthor } from '../../utils/requestBodyToModelBody'
+import ConvertPdf from '../../utils/convertPdf'
+
+const getFilename = (a, b) => {
+  const str = `${a} - ${b}`
+  return str.replace(/[^A-Za-z0-9\-\ ]/g, '')
+}
 
 const checkAdmin = sess =>
   new Promise((resolve, reject) => {
@@ -16,17 +23,48 @@ const checkAdmin = sess =>
 module.exports = function(router) {
   'use strict'
   router.route('/:email').post((req, res, next) => {
-    const sess = req.session
-    if (sess.user) {
-      checkAdmin(sess)
-        .then(() => {
-          return res.status(202).json(req.params.email)
-        })
-        .catch(err => {
-          return res.status(401).json('auth error')
-        })
-    } else {
-      return res.status(404).json('error')
+    try {
+      const sess = req.session
+      const { email } = req.params
+      if (sess.user) {
+        checkAdmin(sess)
+          .then(() => {
+            Registration.find({ 'delegate.email': email }, (err, result) => {
+              if (err) throw err
+              if (result[0] && result[0].paper && result[0].paper.title && result[0].paper.authors && result[0].paper.affiliations && result[0].paper.abstract && result[0].paper.keywords) {
+                const { paper } = result[0]
+                const filename = getFilename(firstAuthor(paper.authors), paper.title)
+                const link = filename ? `/papers/${filename}.pdf` : ''
+
+                ConvertPdf(filename, paper)
+                  .then(response => {
+                    console.log('========== CONVERT SUCCESSFULLY ==========', response)
+                    Registration.update(
+                      { 'delegate.email': email },
+                      { $set: { 'paper.link': link } },
+                      (e3, r3) => {
+                        if (e3) throw e3
+                        return res.status(202).json(r3)
+                      }
+                    )
+                  })
+                  .catch(e2 => {
+                    console.log('========== CONVERT ERR ==========', e2)
+                    throw "Convert PDF Error: Server's converter error! Please inform the admin to fix it."
+                  })
+              } else {
+                return res.status(200).json('OK')
+              }
+            })
+          })
+          .catch(err => {
+            return res.status(401).json('auth error')
+          })
+      } else {
+        return res.status(404).json('error')
+      }
+    } catch (e) {
+      return res.status(500).json('server error')
     }
   })
 }
